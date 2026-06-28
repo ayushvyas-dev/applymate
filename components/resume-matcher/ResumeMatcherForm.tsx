4351ae101cd35083ad54c32e2c1cf6e31f25147f
ayushvyas-dev <ayushvyas909@gmail.com>
2026-06-28 23:32:51 +0530

    update resume-matcher form seperator

--- components/resume-matcher/ResumeMatcherForm.tsx
+++ components/resume-matcher/ResumeMatcherForm.tsx
@@ -171,7 +171,7 @@ export default function ResumeMatcherForm({
             <div className='relative my-2'>
               <Separator />
               <span className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-card px-3 text-xs text-muted-foreground'>
-                or paste description
+                or
               </span>
             </div>
 
